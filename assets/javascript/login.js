f44c31e03dec6eadb4e15f76bf81d45df29708ca
HaroldCiro30 <hciro@josegalan.edu.co>
2023-11-21 00:51:04 -0500

    Se arreglo el inconveniente para iniciar sesión

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -3,7 +3,8 @@ function login() {
     const contrasena = document.getElementById('contrasena').value;
 
     // Verificar si el usuario existe en el localStorage
-    const usuarioGuardado = localStorage.getItem(correo);
+    // const usuarioGuardado = localStorage.getItem(correo);
+    const usuarioGuardado = localStorage.getItem(email);
 
     if (usuarioGuardado) {
         // Si el usuario existe, obtener los datos almacenados
