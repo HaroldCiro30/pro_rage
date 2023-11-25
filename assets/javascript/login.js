d55456febaaaaa21300141b47bfd19c0917959d3
HaroldCiro30 <hciro@josegalan.edu.co>
2023-11-25 01:57:38 -0500

    Se arreglo el login; Añadiendo mas estetica a la pagina principal!.

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -1,9 +1,9 @@
 function login() {
+    event.preventDefault();
     const email = document.getElementById('correo').value;
     const contrasena = document.getElementById('contrasena').value;
 
     // Verificar si el usuario existe en el localStorage
-    // const usuarioGuardado = localStorage.getItem(correo);
     const usuarioGuardado = localStorage.getItem(email);
 
     if (usuarioGuardado) {
@@ -13,20 +13,35 @@ function login() {
         // Verificar si la contraseña coincide
         if (usuario.contrasena === contrasena) {
             alert('Inicio de sesión exitoso');
-            window.location.href = '/views/pagprincipal.html';
+            window.location.href = '../views/pagprincipal.html';
         } else {
             // Contraseña incorrecta
             mostrarError('Contraseña incorrecta');
+            resetearBotonDespuesDeError();
         }
     } else {
         // Usuario no encontrado
         mostrarError('Usuario no encontrado. Por favor, regístrate.');
+        resetearBotonDespuesDeError();
     }
 }
 
 function mostrarError(mensaje) {
     const errorDiv = document.getElementById('error-message');
     errorDiv.textContent = mensaje;
+    resetearBotonDespuesDeError(); // También restablecer el botón aquí en caso de que haya un mensaje previo.
+}
+
+function resetearBotonDespuesDeError() {
+    // Después de 2 segundos, borra el mensaje de error y restablece el botón
+    setTimeout(() => {
+        const errorDiv = document.getElementById('error-message');
+        errorDiv.textContent = '';
+
+        // Restablecer el estado del botón si es necesario
+        const boton = document.getElementById('error-message'); // Reemplaza 'tu-boton-id' con el ID de tu botón
+        boton.removeAttribute('disabled'); // Si el botón tiene un atributo 'disabled', restablecerlo
+    }, 2000);
 }
 
 function back() {
--- assets/javascript/registro.js
+++ assets/javascript/registro.js
@@ -1,23 +1,29 @@
 function register() {
-
+  console.log('La función register se está ejecutando.');
   const nombre = document.getElementById('nombres').value;
   const apellido = document.getElementById('apellidos').value;
   const correo = document.getElementById('correo').value;
   const contrasena = document.getElementById('contrasena').value;
   const sexo = document.getElementById('genero').value;
 
+  // Verificar si alguno de los campos está vacío
+  if (nombre === '' || apellido === '' || correo === '' || contrasena === '' || sexo === '') {
+    alert('Por favor, complete todos los campos.');
+    return; // No registramos si hay campos vacíos
+  }
+
   const usuario = {
-      nombre,
-      apellido,
-      correo,
-      sexo,
-      contrasena
+    nombre,
+    apellido,
+    correo,
+    sexo,
+    contrasena
   };
 
   localStorage.setItem(correo, JSON.stringify(usuario));
 
   alert('Usuario creado correctamente. Por favor, inicie sesión.');
-  window.location.href = '/index.html';
+  window.location.href = '../index.html';
 }
 
 function back() {
